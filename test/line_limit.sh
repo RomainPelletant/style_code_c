d0a3c03d4e69d44af1b40b84b1b16c38c94f18f5
Melvin Vermeeren <mail@mel.vin>
2018-01-30 19:30:56 +0100

    add tests for line_limit

---
+++ test/line_limit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function msg_v()
+{
+	[[ $VERBOSE ]] && printf '%s\n' "$1"
+}
+
+unset VERBOSE
+[[ "$1" == '-v' || "$1" == '--verbose' ]] && VERBOSE=true
+
+TARGET='line_limit/pass'
+TMP="$(../line_limit.sh -v -i "$TARGET")"
+msg_v "$TMP"
+if [[ $(echo "$TMP" | grep -E '^[A-Z]+ +FAIL ') ]]
+then
+	echo "test for '$TARGET' failed"
+	exit 1
+fi
+msg_v "test for '$TARGET' passed"
+
+TARGET='line_limit/fail/limit'
+TMP="$(../line_limit.sh -v "$TARGET")"
+msg_v "$TMP"
+if [[ $(echo "$TMP" | grep -E 'LIMIT +PASS ') ]]
+then
+	echo "test for '$TARGET' failed"
+	exit 1
+fi
+msg_v "test for '$TARGET' passed"
+
+TARGET='line_limit/fail/indent'
+TMP="$(../line_limit.sh -v -i "$TARGET")"
+msg_v "$TMP"
+if [[ $(echo "$TMP" | grep -E '^INDENT +PASS ') ]]
+then
+	echo "test for '$TARGET' failed"
+	exit 1
+fi
+msg_v "test for '$TARGET' passed"
+
+exit 0
